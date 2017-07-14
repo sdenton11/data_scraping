d8980e94c379064d56929a3951789bdef54b2d35
sdenton11 <smd2202@columbia.edu>
2017-07-14 16:30:43 -0700

    get full stats

---
+++ basketball/mvp_logistic_regression.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import numpy as np
+import statsmodels.api as sm
+
+df = pd.read_csv('/Users/samdenton/github/data_scraping/basketball/mvp_totals_2017.csv',
+                        encoding='latin-1')
+
+log_df = df[['MVP', 'AST%', 'BLK%', 'DRB%', 'ORB%', 'TOV%', 'eFG%']]
+
+print (log_df.columns.values)
+
+for column in log_df:
+    log_df[column] = pd.to_numeric(log_df[column], errors='coerce')
+log_df = log_df.dropna()
+
+log_df['intercept'] = 1
+
+train_cols = log_df.columns[1:]
+
+print (train_cols)
+
+logit = sm.Logit(log_df['MVP'], log_df[train_cols])
+
+result = logit.fit()
+
+print (result.summary())
